7451f428d5f882e644eee33a7494ac4736ae974d
Shashank Ranjan SJSU <shashank.ranjan@sjsu.edu>
2026-09-13 21:35:23 -0700

    added files for root direcotry

---
+++ sitecustomize.py
@@ -0,0 +1,11 @@
+# sitecustomize.py
+# Automatically add the project root folder to sys.path
+# Fix for Microsoft Store Python not including working directory.
+
+import sys
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
